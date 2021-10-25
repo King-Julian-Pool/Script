d1acd1f5e7d10bcd191c5f0f4574def5f230075c
King-Julian-Pool <78912385+King-Julian-Pool@users.noreply.github.com>
2021-10-25 19:32:31 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,4 +1,3 @@
-  GNU nano 5.4                                                               backup.sh                                                                        
 #!/bin/bash
 
 #déclaration variable generiques
